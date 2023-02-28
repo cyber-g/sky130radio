955c7f5ce28b2df21fd1552590a638aafd04e474
Cyber-G <cygerpham@free.fr>
2023-02-28 09:20:17 +0100

    gen_gm_id_plots.py: improve plots: enable grid

--- utils/gen_gm_id_plots.py
+++ utils/gen_gm_id_plots.py
@@ -52,15 +52,19 @@ def init_plots():
     figs[0].suptitle('Id/W vs gm/Id')
     plts[0].set_xlabel("gm/Id")
     plts[0].set_ylabel("Id/W")
+    plts[0].grid()
     figs[1].suptitle('fT vs gm/Id')
     plts[1].set_xlabel("gm/Id")
     plts[1].set_ylabel("f_T")
+    plts[1].grid()
     figs[2].suptitle('gm/gds vs gm/Id')
     plts[2].set_xlabel("gm/Id")
     plts[2].set_ylabel("gm/gds")
+    plts[2].grid()
     figs[3].suptitle('gm/Id vs Vgg')
     plts[3].set_xlabel("Vgg")
     plts[3].set_ylabel("gm/Id")
+    plts[3].grid()
     return figs, plts
 
 def gen_plots(gm_id, id_W, ft, gm_gds, vsweep, fet_W, fet_L, plts):
